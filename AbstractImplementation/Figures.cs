522b374de57d74b0f1acb5978b90d8ecdb5da5d1
daiyankabir <kabirdaiyan@gmail.com>
2021-05-06 14:23:06 +0600

    initialization

---
+++ AbstractImplementation/Figures.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractImplementation
+{
+    abstract class Figures
+    {
+        public double width, height, radius;
+        public const float PI = 3.14f;
+
+        public abstract void GetArea();
+    }
+
+    class Rectangle : Figures
+    {
+        public override void GetArea()
+        {
+            Console.WriteLine(height*width);
+        }
+        public Rectangle(double w, double h)
+        {
+            this.height = h;
+            this.width = w;
+        }
+    }
+
+    class Circle : Figures
+    {
+        public override void GetArea()
+        {
+            Console.WriteLine(PI*radius*radius);
+        }
+        public Circle(double r)
+        {
+            this.radius = r;            
+        }
+    }
+
+    class Triangle : Figures
+    {
+        public override void GetArea()
+        {
+            Console.WriteLine(0.5f*height*width);
+        }
+        public Triangle(double w, double h)
+        {
+            this.height = h;
+            this.width = w;
+        }
+    }
+
+    class TestFigures
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(10, 20);
+            Circle c = new Circle(10);
+            Triangle t = new Triangle(10, 20);            
+            r.GetArea(); c.GetArea();t.GetArea();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AbstractImplementation/Interface1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractImplementation
+{
+    interface Interface1
+    {
+        //void Add(int x,int y);
+        void Show();
+    }
+    interface Interface2
+    {
+        //void Sub(int x, int y);
+        void Show();
+    }
+
+    class ImplementationClass : Interface1, Interface2
+    {        
+        static void Main(string[] args)
+        {
+            ImplementationClass IC = new ImplementationClass();
+            Interface1 i1 = IC;
+            i1.Show();
+
+            Console.ReadKey();
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Show Method Invoked!");
+        }
+    }
+}
